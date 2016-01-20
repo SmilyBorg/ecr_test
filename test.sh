4451baa3cae35d3af8d87c12c0484b81fcf608c6
root <root@ip-172-31-21-235.us-west-2.compute.internal>
2016-01-20 09:30:41 +0000

    fixed test_dir

--- test.sh
+++ test.sh
@@ -7,13 +7,13 @@ $ECR_LOGIN
 TEST_DIR=`pwd`
 
 #ecr_test/ruby_parent
-cd $pwd/parent/
+cd $TEST_DIR/parent
 docker build -t ecr_test/ruby_parent .
 docker tag ecr_test/ruby_parent:latest 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_parent:latest
 docker push 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_parent:latest
 
 #ecr_test/ruby_child
-cd $pwd/child
+cd $TEST_DIR/child
 docker build -t ecr_test/ruby_child .
 docker tag ecr_test/ruby_child:latest 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:latest
 docker push 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:latest
