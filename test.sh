6f6ab670c9ea2b25915d1814a61091f589556439
root <root@ip-172-31-41-163.us-west-2.compute.internal>
2016-02-09 12:53:27 +0000

    Added tag and version test cases for both docker hub and ecr

--- test.sh
+++ test.sh
@@ -1,20 +1,73 @@
 #!/bin/bash
 
-pip install --upgrade awscli
+PATH=$PATH:/usr/local/bin:/usr/local/sbin
+
+#pip install --upgrade awscli
 ECR_LOGIN=`aws ecr get-login --region us-east-1`
 $ECR_LOGIN
 
 TEST_DIR=`pwd`
 
+echo
+echo ----------------------------------
+echo ecr_test/ruby_parent:bob
+
 #ecr_test/ruby_parent
-cd $TEST_DIR/parent
-docker build -t ecr_test/ruby_parent .
-docker tag ecr_test/ruby_parent:latest 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_parent:latest
-docker push 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_parent:latest
+cd $TEST_DIR/ecr/parent
+docker build -t 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_parent:bob .
+#docker tag ecr_test/ruby_parent:latest 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_parent:bob
+docker push 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_parent:bob
+
+echo
+echo ----------------------------------
+echo ecr_test/ruby_child:bob
 
 #ecr_test/ruby_child
-cd $TEST_DIR/child
-docker build -t ecr_test/ruby_child .
-docker tag ecr_test/ruby_child:latest 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:latest
-docker push 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:latest
+cd $TEST_DIR/ecr/child
+docker build -t 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:bob .
+#docker tag ecr_test/ruby_child:latest 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:bob
+docker push 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:bob
 
+echo
+echo ----------------------------------
+echo ecr_test/ruby_child:sam
+
+#ecr_test/ruby_child
+cd $TEST_DIR/ecr/child/sam/
+docker build -t 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:sam .
+#docker tag ecr_test/ruby_child:latest 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:bob
+docker push 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:sam
+
+echo
+echo ----------------------------------
+echo smilyborg/ruby_parent:bob
+
+#dockerhub/ruby_parent
+cd $TEST_DIR/dockerhub/parent
+docker build -t smilyborg/ruby_parent:bob .
+#docker tag ecr_test/ruby_parent:latest 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_parent:latest
+docker push smilyborg/ruby_parent:bob
+
+echo
+echo ----------------------------------
+echo smilyborg/ruby_child:bob
+
+#ecr_test/ruby_child
+cd $TEST_DIR/dockerhub/child
+docker build -t smilyborg/ruby_child:bob .
+#docker tag ecr_test/ruby_child:latest 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:latest
+docker push smilyborg/ruby_child:bob
+
+echo
+echo ----------------------------------
+echo smilyborg/ruby_child:sam
+
+#ecr_test/ruby_child
+cd $TEST_DIR/dockerhub/child/sam/
+docker build -t smilyborg/ruby_child:sam .
+#docker tag ecr_test/ruby_child:latest 231879086156.dkr.ecr.us-east-1.amazonaws.com/ecr_test/ruby_child:latest
+docker push smilyborg/ruby_child:sam 
+
+echo
+echo ----------------------------------
+echo
